6af3e096038e80ef1e616e5fd9cb321a1a5b6ea3
Jeff Kamo <kamojeffrey@gmail.com>
2016-11-02 00:10:23 -0700

    Add get:data nodes cript to generate game data

---
+++ bin/generate-game-data.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+var path = require('path');
+var connect = require('../scripts/connect');
+var fileUtils = require('../scripts/file-utils');
+
+var write = function(filename, object) {
+    var pathToWrite = __dirname + '/../app/data/' + filename;
+
+    try {
+        fileUtils.jsonWrite(pathToWrite)(object);
+    } catch(error) {
+        console.log(chalk.red('Problem in: ' + __dirname + '/generate-game-data.js'));
+        console.log(chalk.red(error));
+    }
+}
+
+setTimeout(function() {
+    var abilities = connect('jobAbilities');
+    var characters = connect('characters');
+    var bestiary = connect('bestiary');
+    var script = connect('script');
+
+    write('abilities.json', abilities);
+    write('characters.json', characters);
+    write('bestiary.json', bestiary);
+    write('script.json', script);
+}, 2000);
